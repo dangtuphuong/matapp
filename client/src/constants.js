614ae1c73e2b0d6e7eb4e780a2b0740f6e74eef1
Phuong Dang <phuongtdangg@gmail.com>
2025-04-18 15:49:51 +1000

    fix labels

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,11 @@
-// User roles stored as numeric values for easier DB handling: 0 = Admin, 1 = Normal User, 2 = Premium User
 export const ROLES = {
   ADMIN: "admin",
   NORMAL_USER: "normal_user",
   PREMIUM_USER: "premium_user",
+};
+
+export const ROLE_LABELS = {
+  [ROLES.ADMIN]: "Admin",
+  [ROLES.NORMAL_USER]: "Normal User",
+  [ROLES.PREMIUM_USER]: "Premium User",
 };
