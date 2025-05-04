62af39b65a22c9aa9b7012966236c38af2bab1ad
Phuong Dang <phuongtdangg@gmail.com>
2025-05-04 15:19:41 +1000

    revert change

--- server/services/machine_learning/deepseek_service.py
+++ server/services/machine_learning/deepseek_service.py
@@ -1,7 +1,7 @@
 import os
 import json
 from dotenv import load_dotenv
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import (
     ChatPromptTemplate,
