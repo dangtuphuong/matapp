b9ec68f067839a75a461d30455b285345cdb0d83
NamDaoVuHoang <47967350+NamDaoVuHoang@users.noreply.github.com>
2025-05-12 19:56:14 +1000

    fix ip

--- client/src/services/smart-search-service.js
+++ client/src/services/smart-search-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = "http://127.0.0.1:5000/api";
 
 export const vectorSearch = async (query, limit, skip) => {
   const token = localStorage.getItem("access_token");
