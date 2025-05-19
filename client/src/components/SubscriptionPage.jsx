e6fdd6008f3498956e6a421f85125dd96398f5c5
Korn <104338734@student.swin.edu.au>
2025-05-19 11:57:01 +1000

    remove all students contact

--- client/src/components/SubscriptionPage.jsx
+++ client/src/components/SubscriptionPage.jsx
@@ -13,38 +13,7 @@ import {
 } from "@mui/material";
 import EmailIcon from "@mui/icons-material/Email";
 
-const teamMembers = [
-  {
-    name: "Aditya Roy",
-    id: "104671426",
-    email: "104671426@student.swin.edu.au",
-  },
-  {
-    name: "Lalitha Samudith",
-    id: "104760748",
-    email: "104760748@student.swin.edu.au",
-  },
-  {
-    name: "Vu Hoang Nam Dao",
-    id: "104474191",
-    email: "104474191@student.swin.edu.au",
-  },
-  {
-    name: "Pattarapol Laovanich",
-    id: "104338734",
-    email: "104338734@student.swin.edu.au",
-  },
-  {
-    name: "Riya Shrestha",
-    id: "104652997",
-    email: "104652997@student.swin.edu.au",
-  },
-  {
-    name: "Tu Phuong Dang",
-    id: "103814482",
-    email: "103814482@student.swin.edu.au",
-  },
-];
+const teamMembers = [];
 
 const SubscriptionModal = ({ open, handleClose }) => {
   const handleEmailClick = (email) => {
@@ -55,21 +24,11 @@ const SubscriptionModal = ({ open, handleClose }) => {
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle>Contact Our Team</DialogTitle>
       <DialogContent>
-        <Typography variant="body1" paragraph>
-          For subscription inquiries, please contact one of our team members
-          below:
+        <Typography variant="body1" paragraph align="center">
+          Please contact website admin for more info.
         </Typography>
-        <List>
-          {teamMembers.map((member, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={member.name} />
-              <IconButton onClick={() => handleEmailClick(member.email)}>
-                <EmailIcon />
-              </IconButton>
-            </ListItem>
-          ))}
-        </List>
       </DialogContent>
+
       <DialogActions>
         <Button onClick={handleClose} color="primary">
           Close
